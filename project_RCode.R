9a46531f42a7b4f2cafbfd7e9a901032c517076a
rroggenk <rroggenk@calpoly.edu>
2022-04-30 00:26:14 -0700

    updated split data and matrix scatterplot with nnew dataset

--- project_RCode.R
+++ project_RCode.R
@@ -1,8 +1,10 @@
 library(tidyverse)
-realestate <- read_csv("Real_Estate.csv")
+library(readxl)
+realestate <- read_excel("SLO_Real_Estate_Feb-Apr2022.xlsx")
+
 
 # IV. Split the Data
-n <- 522
+n <- 144
 numtrain = ceiling(.8*n)
 set.seed(678)
 train_ind = sample(n, numtrain)
@@ -12,4 +14,8 @@ set.seed(NULL)
 
 
 # V. Data Visualization
-pairs(Price ~ Sqft + Bed + Bath + Age + Garage, data = traindata, col = c('cornflowerblue','royalblue')[factor(traindata$Quality)], pch = 19)
+pairs(SoldPrice ~ Sqft + Bed + Bath + YearBuilt + LotSize + Parking, data = traindata, 
+      col = c('aquamarine', 'cornflowerblue','royalblue', 'cadetblue', 'cyan', 
+              'darkblue', 'deepskyblue', 'darkseagreen', 'darkseagreen1')[factor(traindata$HomeType)], 
+      pch = 19)
+
