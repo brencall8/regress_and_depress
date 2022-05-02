1fa729d13d0c29372c97f1b4b28e01639c6b3f53
rroggenk <rroggenk@calpoly.edu>
2022-05-01 23:43:00 -0700

    added more plots

--- project_RCode.R
+++ project_RCode.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(readxl)
+library(rgl)
+library(scatterplot3d)
 realestate <- read_excel("SLO_Real_Estate_Feb-Apr2022.xlsx")
 
 
@@ -41,7 +43,7 @@ pairs(SoldPrice ~ Sqft + Bed + Bath + YearBuilt + LotSize + Parking, data = trai
       pch = 19)
 
 
-# Interaction Plot 
+# Interaction Plot Between Sold Price and Square Footage (Home Type)
 interaction = aov(SoldPrice ~ Sqft*HomeType, data = traindata)
 interaction.plot(x.factor = traindata$Sqft, #x-axis variable
                  trace.factor = traindata$HomeType, #variable for lines
@@ -62,19 +64,52 @@ traindata %>%
   geom_point()
 
 
-
 # Histograms of Quantitative Variables 
 traindata %>%
   ggplot(aes(x = Sqft, fill = HomeType)) +
-  geom_histogram()
+  geom_histogram() +
+  scale_fill_manual(values = c('aquamarine', 'cornflowerblue','royalblue', 'cadetblue', 'cyan', 
+                               'darkblue', 'deepskyblue', 'darkseagreen', 'darkseagreen1'))
 
 traindata %>%
   ggplot(aes(x = YearBuilt, fill = Pool)) +
-  geom_histogram(binwidth = 10)
+  geom_histogram(binwidth = 10) +
+  scale_fill_manual(values = c('royalblue', 'cyan'))
 
 traindata2 %>%
   ggplot(aes(x = LotSize, fill = HomeType)) +
-  geom_histogram(binwidth = 2000)
+  geom_histogram(binwidth = 2000) +
+  scale_fill_manual(values = c('aquamarine', 'cornflowerblue','royalblue', 'cadetblue', 'cyan', 
+                               'darkblue', 'deepskyblue', 'darkseagreen', 'darkseagreen1'))
+
+
+# Interaction Plot Between TWO CATEGORICAL VARIABLES 
+
+
+# 3D Scatterplot 
+# with(traindata, plot3d(x=Sqft, y=SoldPrice, z=YearBuilt, pch=10,size=8))
+# 
+# #regression surface (additive model)
+# surface<-function(x1,x2){
+#   cars.fit2$coeff[1]+cars.fit2$coeff[2]*x1+cars.fit2$coeff[3]*x2
+# }
+# 
+# with(traindata,{
+#   x1=seq(min(Sqft),max(Sqft),length=15)
+#   x2=seq(min(SoldPrice),max(SoldPrice),length=100)
+#   y=outer(x1,x2,surface)
+#   surface3d(x1,x2,y,alpha=0.5,col='lightblue',front='lines', back='lines')
+# })
+
+
+s3d <- scatterplot3d(traindata$Sqft, y=traindata$SoldPrice, z=traindata$YearBuilt,
+                     xlab = "Square Footage (sqft)",
+                     ylab = "Sold Price (Dollars)",
+                     zlab = "Year Built", 
+                     angle=55, pch = 16)
+# Add regression plane
+my.lm <- lm(traindata$Sqft ~ traindata$SoldPrice + traindata$YearBuilt)
+s3d$plane3d(my.lm)
 
 
 
