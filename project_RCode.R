a4bf369f6e4ea7a7773a1d41967bc85a5d38925e
rroggenk <rroggenk@calpoly.edu>
2022-05-25 21:39:12 -0700

    parts 5b, 7, and 8

--- project_RCode.R
+++ project_RCode.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(readxl)
 library(scatterplot3d)
+library(GGally)
+library(car)
+library('lmtest')
 realestate <- read_excel("SLO_Real_Estate_Feb-Apr2022.xlsx")
 
 
@@ -24,8 +27,6 @@ pairs(SoldPrice ~ Sqft + Bed + Bath + YearBuilt + LotSize + Parking, data = trai
       pch = 19)
 
 
-# VI. Variable Pre-Processing
-
 # Matrix Scatterplot (Without 3 High Leverage Points)
 lotsize_minus_high_lev <- realestate %>%
   filter(LotSize != 416869 & LotSize != 108900 & LotSize != 84071)
@@ -47,19 +48,37 @@ pairs(SoldPrice ~ Sqft + Bed + Bath + YearBuilt + LotSize + Parking, data = trai
 cor(traindata[,c(1, 2, 3, 4, 5, 6, 7)])
 cor(traindata2[,c(1, 2, 3, 4, 5, 6, 7)])
 
+
+# Matrix Scatterplot with ggpairs()
+ggpairs(traindata[,-c(8:12)], 
+        upper=list(continuous='points'),
+        lower=list(continuous='cor',combo='blank',discrete='blank'))
+
+
 # Interaction Plot Between Sold Price and Square Footage (Home Type)
-interaction = aov(SoldPrice ~ Sqft*HomeType, data = traindata)
-interaction.plot(x.factor = traindata$Sqft, #x-axis variable
-                 trace.factor = traindata$HomeType, #variable for lines
-                 response = traindata$SoldPrice, #y-axis variable
-                 fun = median, #metric to plot
-                 ylab = "Sold Price",
-                 xlab = "Square Footage (sqft)",
-                 col = c('aquamarine', 'cornflowerblue','royalblue', 'cadetblue', 'cyan', 
-                         'darkblue', 'deepskyblue', 'darkseagreen', 'darkseagreen1'),
-                 lty = 1, #line type
-                 lwd = 2, #line width
-                 trace.label = "Home Type")
+traindata$HomeType=factor(traindata$HomeType)
+traindata <- traindata %>%
+  mutate(HomeTypeNew = 
+           case_when(HomeType == "Condominium" ~ "Condo", 
+                     HomeType == "SingleFamilyResidence" ~ "SingleFam",
+                     HomeType == "ManufacturedHome" ~ "Manufactured",
+                     TRUE ~ "Other")) 
+traindata$HomeTypeNew=factor(traindata$HomeTypeNew)
+fit_interaction = lm(SoldPrice~Sqft*HomeTypeNew, data=traindata)
+summary(fit_interaction)
+
+plot(SoldPrice ~ Sqft, data = traindata, 
+     col = c('cornflowerblue','royalblue','cyan', 'darkseagreen')[HomeTypeNew], 
+     pch = c(15, 16, 17, 18))
+legend('bottomright',legend=levels(traindata$HomeTypeNew), 
+       pch = c(15, 16, 17, 18), 
+       col = c('cornflowerblue','royalblue','cyan', 'darkseagreen'), 
+       lty = 1)
+coeff = fit_interaction$coeff
+abline(coeff[1], coeff[2], col='cornflowerblue')
+abline(coeff[1] + coeff[3], coeff[2] + coeff[6], col = 'royalblue') 
+abline(coeff[1] + coeff[4], coeff[2] + coeff[7], col = 'cyan') 
+abline(coeff[1] + coeff[5], coeff[2] + coeff[8], col = 'seagreen') 
 
 
 # Scatterplot of Sold Price and Sqft, Colored By Home Type
@@ -87,7 +106,11 @@ traindata2 %>%
                                'darkblue', 'deepskyblue', 'darkseagreen', 'darkseagreen1'))
 
 
-# Interaction Plot Between TWO CATEGORICAL VARIABLES 
+# Interaction Plot Between Home Type and Pool
+traindata$Pool=factor(traindata$Pool)
+with(traindata, interaction.plot(HomeTypeNew, Pool, log(SoldPrice), type = 'b', 
+                                 col = c('royalblue','cyan'), 
+                                 pch = c(16, 17)))
 
 
 # 3D Scatterplot 
@@ -101,4 +124,29 @@ my.lm <- lm(traindata2$Sqft ~ traindata2$SoldPrice + traindata2$LotSize)
 s3d$plane3d(my.lm)
 
 
+
+# VII. Residual Analysis 
+par(mfrow=c(2,2)) #plot in 2 by 2 grid   
+plot(final_model)
+
+plot(resid(final_model) ~ log(SoldPrice), data=traindata, ylab='Residuals', xlab='log(SoldPrice)')
+abline(h=0,lty=2)
+
+plot(resid(final_model) ~ YearBuilt, data=traindata, ylab='Residuals', xlab='YearBuilt')
+abline(h=0,lty=2)
+
+plot(resid(final_model) ~ HomeTypeNew, data=traindata, ylab='Residuals', xlab='HomeTypeNew')
+abline(h=0,lty=2)
+
+hist(resid(final_model), main='', xlab='Residuals')
+
+shapiro.test(resid(final_model))
+bptest(final_model)
+
+
+
+# VIII. Fit a Linear Model 
+final_model = lm(I(log(SoldPrice)) ~ I(log(Sqft)) + YearBuilt + HomeTypeNew, data = traindata)
+summary(final_model)
+vif(final_model)
 
